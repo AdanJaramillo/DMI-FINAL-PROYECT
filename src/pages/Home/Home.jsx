ef8a09089c22432c66749ce7699fd30216865d53
Eddsalasgit <edslgn.1234@gmail.com>
2022-04-13 22:14:18 -0500

    Intento de camara en donde se caga la imagen desde el gestor de archivos y se abre la camara para tomar la imagen.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Text, View, Linking } from 'react-native';
+import { Text, View, Linking, Platform } from 'react-native';
 import {styles} from "./Home.styles";
 import ButtonComponent from "../../components/Button";
 import i18n from "../../../localization/i18n"
 import {Amplify} from "aws-amplify"
 import {Auth} from "aws-amplify";
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
 
 export default function HomeScreen(){
 
@@ -25,14 +27,65 @@ export default function HomeScreen(){
     .catch(err => console.log(err));
   }
 
+  //---------- Eduardo Camara -------------
+  const [image, setImage] = useState(null);
+
+  useEffect(async()=>{
+    if(Platform.OS !== "web"){
+      const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if(status !== 'granted'){
+        alert('Permission denied!')
+      }
+    }
+  },[])
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const openCamera = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false){
+      alert("You've refused to allow this app to access your camera!");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync();
+
+    console.log(result);
+
+    if (!result.cancelled){
+      setImage(result.uri);
+      console.log(result.uri);
+    }
+  }
+//---------------------------------------------------------
     return (
       <View style={styles.container}>
       <Text style={{color: 'blue'}}
         onPress={() => Linking.openURL("https://github.com/AdanJaramillo/DMI-FINAL-PROYECT")}>
           GitHub
         </Text>
-
+{/* Eduardo Camara */}
+      <ButtonComponent title="Pick an image from camera roll" onPress={pickImage}/>
+      <ButtonComponent onPress={openCamera} title="Open Camera"/>
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+{/* Eduardo Camara */}
       
+
         <Text>{i18n.t("Home Screen")}</Text>
         <ButtonComponent title={i18n.t("Logout")} onPress={signOut} />
         
